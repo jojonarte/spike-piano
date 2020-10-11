9e962640ce3278db9027be60ed5457f53efb881f
jojonarte <joselitonarte@gmail.com>
2020-10-12 00:12:45 +0800

    feat: Soundfont provider

---
+++ src/adapters/Soundfont/SoundfontProvider.ts
@@ -0,0 +1,29 @@
+import React, { FunctionComponent, ReactElement, useEffect } from 'react';
+import { InstrumentName } from 'soundfont-player';
+import { MidiValue } from '../../domain/note';
+import { useInstrument } from '../../state/Instrument/Context';
+import { useSoundfont } from './useSoundfont';
+interface ProviderProps {
+	instrument?: InstrumentName;
+	AudioContext: AudioContextType;
+	render(props: ProvidedProps): ReactElement;
+}
+
+interface ProvidedProps {
+	loading: boolean;
+	play(note: MidiValue): Promise<void>;
+	stop(note: MidiValue): Promise<void>;
+}
+
+export const SoundfontProvider: FunctionComponent<ProviderProps> = ({
+	AudioContext,
+	instrument,
+	render,
+}) => {
+	const { loading, play, stop, load, current } = useSoundfont({ AudioContext });
+	useEffect(() => {
+		if (!loading && instrument !== current) load(instrument);
+	}, [load, loading, instrument, current]);
+
+	return render({ loading, play, stop });
+};
--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,18 +1,19 @@
 import React, { FunctionComponent, useEffect } from 'react';
+import { SoundfontProvider } from '../../adapters/Soundfont/SoundfontProvider';
 import { useSoundfont } from '../../adapters/Soundfont/useSoundfont';
 import { useAudioContext } from '../../adapters/useAudioContext';
 import { useInstrument } from '../../state/Instrument/Context';
-import { useMount } from '../../utils/useMount';
 import { Keyboard } from './Keyboard';
 
 export const KeyboardWithInstrument: FunctionComponent = () => {
 	const AudioContext = useAudioContext()!;
 	const { instrument } = useInstrument();
-	const { loading, play, stop, load, current } = useSoundfont({ AudioContext });
 
-	useEffect(() => {
-		if (!loading && instrument !== current) load(instrument);
-	}, [load, loading, current, instrument]);
-
-	return <Keyboard loading={loading} play={play} stop={stop} />;
+	return (
+		<SoundfontProvider
+			AudioContext={AudioContext}
+			instrument={instrument}
+			render={(props) => <Keyboard {...props} />}
+		/>
+	);
 };
